76bc1de4d369a332921c7dac925396263a45dbd1
swakswak <hschoi0702@gmail.com>
2022-11-21 01:11:09 +0900

    Program.cs 수정

--- DatabaseLockSample.EFCore/Program.cs
+++ DatabaseLockSample.EFCore/Program.cs
@@ -1,20 +1,25 @@
-﻿using DatabaseLockSample.EFCore;
-using DatabaseLockSample.EFCore.Models;
-using DatabaseLockSample.EFCore.Services;
+﻿using DatabaseLockSample.EFCore.Models;
 using DatabaseLockSample.EFCore.Services.Optimistic;
 
 var optimisticLockBookService = new OptimisticLockLibraryService();
-var registered = await optimisticLockBookService.RegisterAsync(Book.Register($"TEST-BOOK-{Guid.NewGuid().ToString().Split("-")[0]}", 1));
+
+var newBook = Book.Register(
+    $"TEST-BOOK-{Guid.NewGuid().ToString().Split("-")[0]}",
+    1
+);
+var registered = await optimisticLockBookService.RegisterAsync(newBook);
 Console.WriteLine($"registered=({registered})");
 
-var checkoutParam = await optimisticLockBookService.GetAsync(registered.Name);
-Console.WriteLine($"ddd=({checkoutParam})");
-
-var checkedOutBook = await optimisticLockBookService.CheckOutAsync(checkoutParam!);
+var checkedOutBook = await optimisticLockBookService.CheckOutAsync((await optimisticLockBookService.GetAsync(registered.Name))!);
 Console.WriteLine($"checkedOutBook=({checkedOutBook})");
 
 await optimisticLockBookService.ReturnAsync(checkedOutBook);
 
 var afterReturn = await optimisticLockBookService.GetAsync(registered.Name);
+Console.WriteLine($"afterReturn=({afterReturn})");
 
-Console.WriteLine($"registered=({registered}), afterRegister=({checkoutParam}), checkedOutBook=({checkedOutBook}), afterReturn={afterReturn}");
+Console.WriteLine();
+Console.WriteLine("===== result =====");
+Console.WriteLine($"registered.Version={registered.Version}, " +
+                  $"checkedOutBook.Version={checkedOutBook.Version}, " +
+                  $"afterReturn.Version={afterReturn!.Version}");
